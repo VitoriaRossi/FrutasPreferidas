5daca3cdeddcb87581a1bf56f519624d75cdc0e9
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-11-04 16:16:04 -0300

    Adiciona a resposta do exercício.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Minhas frutas preferidas ---\n");
+Console.WriteLine("1.Morango");
+Console.WriteLine("2.Maçã");
+Console.WriteLine("3.Uva");
+Console.WriteLine("4.Manga");
+Console.WriteLine("5.Banana");
